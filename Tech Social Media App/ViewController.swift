01c612a79eca52169950e70ce8f340a7378a444f
Kay-711 <kaylenward22@gmail.com>
2024-10-16 16:29:32 -0500

    Project commit

---
+++ Tech Social Media App/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Tech Social Media App
+//
+//  Created by Kaylen Ward on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
